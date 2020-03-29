47c14de801751c2365ce43cd41c567188a7732fe
yanchen <yanchen.c6@gmail.com>
2020-03-29 15:43:53 +0000

    Clean up printouts and heapsort functions

--- heapsort/heapsort.cc
+++ heapsort/heapsort.cc
@@ -2,34 +2,35 @@
 
 #include "heapsort.h"
 
-int main(int argc, char **argv) {
-  int n;
-  std::cin >> n;
-  int A[n];
-  int B[n];
-  int inputsize = n + n;
-  for (int i = 0; i < inputsize; i++) {
-    int number;
-    std::cin >> number;
-    // std::cout << i;
-    if (i < n) {
-      A[i] = number;
-    } else {
-      B[i - n] = number;
-    }
-  }
-
-  int len = n * n + 1;
-  int toSort[len];
-  toSort[0] = 0;
-  cartisanProduct(toSort, A, B, n);
-  print_array(toSort, len);
-
-  heapSort(toSort, len);
-  print_array(toSort, len);
-
-  for (int i = 1; i <= 5; i++) {
-    std::cout << toSort[i] << ' ';
-  }
-  return 0;
+int main(int argc, char** argv) {
+	int n;
+  	std::cin >> n;
+  	int A[n];
+  	int B[n];
+  	int inputsize = n+n;
+  	for (int i=0; i < inputsize; i++){
+  		int number;
+    	std::cin >> number;
+    	//std::cout << i;
+  		if (i<n){
+        	A[i] = number;
+		  }else{
+		  	B[i-n] = number;
+		  }
+        
+  	}
+  	
+  	int len = n*n+1;
+	int toSort[len];
+	toSort[0] = 0;
+  	cartisanProduct( toSort, A, B, n);
+  	//print_array(toSort, len);
+  	
+  	heapSort(toSort, len);
+  	//print_array(toSort, len);
+  	
+  	for(int i=1; i<=5; i++){
+  		std::cout << toSort[i] << ' ';
+	  }
+	return 0;
 }
